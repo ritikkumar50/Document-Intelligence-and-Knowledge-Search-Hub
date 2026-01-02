ff5818e6267efd63d3c6d5c51aed61bc3657bb98
Ritik Kumar <ritikbnl2020@gmail.com>
2026-01-02 15:49:18 +0530

    demo route add

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Document Intelligence Hub API is running' });
+});
+
 // Routes
 const authRoutes = require('./routes/authRoutes');
 const documentRoutes = require('./routes/documentRoutes');
